14dfb8ea5e77e944243b2515eab1fd3a32472a14
Ryan Paul McClure <ryan333@vt.edu>
2022-02-23 10:45:20 -0500

    Create install.r

---
+++ install.r
@@ -0,0 +1,4 @@
+install.packages(c("tidyverse", "MCMCvis", "lubridate","tidybayes",
+               "ncdf4", "reshape2", "zoo", "patchwork", "hydroGOF", "viridis",
+               "imputeTS", "devtools", "scales", "forecast", "coda", "rjags", "R2jags",
+               "gridExtra", "pacman"))
